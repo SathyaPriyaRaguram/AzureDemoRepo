1bf5b21a10fbe51e45257c85445bef851f9276c3
SathyaPriyaRaguram <sathyapriya.raguram@cognizant.com>
2021-04-12 16:04:34 +0530

    Code files

---
+++ src/Test1.java
@@ -0,0 +1,32 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class Test1 {
+	WebDriver driver;
+  @Test
+  public void f() {	
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\UC269205\\Downloads\\Selenium\\chromedriver_win32_89\\chromedriver.exe");
+  	  driver = new ChromeDriver();
+  	  String url = "https://www.google.com";
+  	  driver.get(url);
+  	  //Capturing the title and validating if expected is equal to actual
+  	  String expectedTitle = "Google";
+  	  String actualTitle = driver.getTitle();
+  	  Assert.assertEquals(actualTitle, expectedTitle);
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Starting the browser session");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("Closing the browser session");
+  	  driver.quit();
+  }
+
+}
